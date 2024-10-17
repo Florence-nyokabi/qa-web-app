92e891d504193a3c81b22f796d9dbd5964986928
Florence-nyokabi <florencenyokabiwangui@gmail.com>
2024-10-17 22:59:38 +0300

    Deployment fixes

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
